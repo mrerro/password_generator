24c33150d9b7a9d9de3bc9bb6f9dbc37df4d3392
mrerro <dyakov.hellvisionstudio@gmail.com>
2015-11-06 16:07:24 +0600

    v1.0
    -loading key from file
    -new interface
    -any bug fix

---
+++ keygen/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace keygen
+{
+
+    public partial class Form1 : Form
+    {
+        readonly Generator Gen = new Generator();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = Gen.generateKey(textBox1.Text);
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            textBox1.ResetText();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() != System.Windows.Forms.DialogResult.OK) return;
+            var lines = new string[2];
+            var file = new System.IO.StreamReader(openFileDialog1.FileName);
+            for (var i = 0; i < lines.Length; i++)
+            {
+                string line;
+                if ((line = file.ReadLine()) != null)
+                {
+                    lines[i] = line;
+                }
+            }
+            Gen.arrayGenerator(lines);
+
+            var outString = "";
+            for (var i = 0; i < Gen.keyArray.GetLength(0); i++)
+            {
+                for (var j = 0; j < Gen.keyArray.GetLength(1); j++)
+                {
+                    outString = outString + Gen.keyArray[i, j] + " ";
+                }
+                outString = outString + "\n";
+            }
+            label1.Text = outString;
+            var arrow = new string[8] { "↑", "↗", "→", "↘", "↓", "↙", "←", "↖" };
+            var temp = Gen.algoritmArray.Aggregate("", (current, item) => current + arrow[item] + " ");
+            label2.Text = temp;
+        }
+    }
+}
---
+++ keygen/Generator.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace keygen
+{
+    internal class Generator
+    {
+        public char[,] keyArray;
+        public int[] algoritmArray;
+        private int stepCount;
+        private int stopKey = 0;
+
+        public string generateKey(string line)
+        {
+            var outLine = "";
+            foreach (var item in line)
+            {
+                for (var i = 0; i < keyArray.GetLength(0); i++)
+                {
+                    for (var j = 0; j < keyArray.GetLength(1); j++)
+                    {
+                        if (item == keyArray[i, j])
+                        {
+                            outLine = outLine + step(i, j);
+                        }
+                    }
+                }
+            }
+            if ((stopKey = stopKey + outLine.Length) < 6)
+            {
+                outLine = outLine + generateKey(line);
+            }
+            return outLine;
+        }
+
+        public void arrayGenerator(string[] lines)
+        {
+            var d = Math.Sqrt(lines[0].Length);
+            var p = (int)d;
+            if (d - p == 0)
+            {
+                keyArray = new char[p, p];
+                algoritmArray = new int[lines[1].Length];
+                var count = 0;
+                for (var i = 0; i < p; i++)
+                {
+                    for (var j = 0; j < p; j++)
+                    {
+
+                        keyArray[i, j] = lines[0].ElementAt(count);
+                        count++;
+                    }
+                }
+            }
+            for (var i = 0; i < lines[1].Length; i++)
+            {
+                algoritmArray[i] = (int)lines[1].ElementAt(i) - 48;
+            }
+        }
+
+        private char step(int i, int j)
+        {
+            while (true)
+            {
+                if (stepCount >= algoritmArray.Length)
+                {
+                    stepCount = 0;
+                }
+                switch (algoritmArray[stepCount])
+                {
+                    case 0:
+                        if (i - 1 != -1)
+                        {
+                            stepCount++;
+                            var temp = i - 1;
+                            return keyArray[temp, j];
+                        }
+                        stepCount++;
+                        continue;
+                    case 1:
+                        if ((i - 1 != -1) && (j + 1 < 6))
+                        {
+                            stepCount++;
+                            return keyArray[i - 1, j + 1];
+                        }
+                        stepCount++;
+                        continue;
+                    case 2:
+                        if (j + 1 < 6)
+                        {
+                            stepCount++;
+                            return keyArray[i, j + 1];
+                        }
+                        stepCount++;
+                        continue;
+                    case 3:
+                        if ((i + 1 < 6) && (j + 1 < 6))
+                        {
+                            stepCount++;
+                            return keyArray[i + 1, j + 1];
+                        }
+                        stepCount++;
+                        continue;
+                    case 4:
+                        if (i + 1 < 6)
+                        {
+                            stepCount++;
+                            var temp = i + 1;
+                            return keyArray[temp, j];
+                        }
+                        stepCount++;
+                        continue;
+                    case 5:
+                        if ((i + 1 < 6) && (j - 1 != -1))
+                        {
+                            stepCount++;
+                            return keyArray[i + 1, j - 1];
+                        }
+                        stepCount++;
+                        continue;
+                    case 6:
+                        if (j - 1 != -1)
+                        {
+                            stepCount++;
+                            return keyArray[i, j - 1];
+                        }
+                        stepCount++;
+                        continue;
+                    case 7:
+                        if ((i - 1 != -1) && (j - 1 != -1))
+                        {
+                            stepCount++;
+                            return keyArray[i - 1, j - 1];
+                        }
+                        stepCount++;
+                        continue;
+                }
+                return ' ';
+            }
+        }
+    }
+}
